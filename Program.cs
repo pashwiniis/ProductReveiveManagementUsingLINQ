55875733cdfe23b4010acc1d7d1bc5c0f5e6743c
pashwiniis <pashwini684@gmail.com>
2022-01-10 15:27:41 +0530

    retrive product id and review

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace ProductReveiveManagementUsingLINQ
 
             while (flag)
             {
-                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating  \n 3. Retrieve Records using Product ID which is Rating > 3\n 4. The Number of Records of ProductID \n 5. Retrieve Product ID and Review\n 6. Skip Top Five Records\n 7. Exit");
+                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating  \n 3. Retrieve Records using Product ID which is Rating > 3\n 4. The Number of Records of ProductID \n 5. Retrieve Product ID and Review\n 6. Skip Top Five Records \n 7. Retrieve Product ID and Review\n 8. Exit");
                 int number = Convert.ToInt32(Console.ReadLine());
                 switch (number)
                 {
@@ -54,6 +54,9 @@ namespace ProductReveiveManagementUsingLINQ
                         Console.WriteLine("Top Five records are Skipped, And Above records are Remaining Records");
                         break;
                     case 7:
+                        management.RetrieveOnlyproductIDAndReview(productList);
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
